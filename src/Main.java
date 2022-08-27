f30f19432814478ae241161386121d4797bbc42d
Ferdoria <ferdoria@gmail.com>
2022-08-27 11:13:36 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args){
+        int param1 = 10;
+        int param2 = 30;
+
+        int valor = suma(param1, param2);
+        System.out.println(valor);
+    }
+    public static int suma(int a, int b){
+        return a + b;
+    }
+}
